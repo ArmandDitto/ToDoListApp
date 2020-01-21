c28f707d38dabe3c6a8fcc5bb2eadf6fc0af632a
Muhamad Armanditto <deepfreeze1197@gmail.com>
2020-01-21 16:15:41 +0700

    Penambahan Float Action Button

--- app/src/main/java/com/ditto/training/todolistapp/MainActivity.java
+++ app/src/main/java/com/ditto/training/todolistapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.ditto.training.todolistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton fabku;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabku = findViewById(R.id.fa_btn);
+        fabku.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
